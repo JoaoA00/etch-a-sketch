a735f4a86c277012b35d2327162b2256f59fee2e
João Abner <joaoabner2022@gmail.com>
2024-11-19 11:42:20 -0300

    Add a button for the size, an erase button and made some script modifications
    Size btn define the squares per side and the erase btn make the squares turn back white when hovered. The code that generate the squares was moved into a function 'newGrid'. The btn size call this function after change the value of squares.

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const content = document.getElementById('container');
-const width = 16;
-const height =  16;
-const proportion = width*height;
+let proportion = 0;
+let userValue = 0;
+let eraseToggle = false;
 
-for(let i = 0; i < proportion; i++){
-    content.innerHTML += '<div class:"square">'+ i +'</div>';
-}
+const btnSize = document.getElementById('btn-size');
+const btnErase = document.getElementById('btn-erase');
 
-const squares = document.querySelectorAll('.square');
-
-content.addEventListener('mouseover', function(e){
-    e.target.style.backgroundColor = 'blue';
+btnErase.addEventListener('click', function(){
+    (eraseToggle) ? eraseToggle = false : eraseToggle = true;
+    console.log(eraseToggle);
 })
+
+btnSize.addEventListener('click', function(){
+    userValue = prompt('Tamanho do grid (max:100):');
+    if(userValue > 100){
+        userValue = 100;
+    }
+    proportion = userValue*userValue;
+    newGrid();
+});
+
+function newGrid(){
+    content.innerHTML = " ";
+
+    for(let i = 0; i < proportion; i++){
+        content.innerHTML += `<div style="width: ${(960 /userValue) - 4}px; height: ${(960 /userValue) - 4}px;"></div>`;
+    }
+
+    content.style.backgroundColor = 'white';
+    
+    content.addEventListener('mouseover', function(e){
+        if(eraseToggle){
+            e.target.style.backgroundColor = 'white';
+        } else {
+            e.target.style.backgroundColor = 'blue';
+        }
+        
+    })
+
+}
 
